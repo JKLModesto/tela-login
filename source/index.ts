6c36f3c7d5cf704ede7968b8a2584caa2817cb47
JKLModesto <pedrogabrielmodesto@gmail.com>
2024-03-11 20:41:46 -0300

    Codigo base TypeScript

---
+++ source/index.ts
@@ -0,0 +1,8 @@
+const username = document.getElementById('username') as HTMLInputElement;
+const password = document.getElementById('password') as HTMLInputElement;
+
+function verifyAutentice():void {
+    
+}
+
+addEventListener('click', verifyAutentice)
