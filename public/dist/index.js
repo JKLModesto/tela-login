4ed9aa580e12369e0aad046552288d5bbaa55f9d
JKLModesto <pedrogabrielmodesto@gmail.com>
2024-03-11 20:43:41 -0300

    run build

---
+++ public/dist/index.js
@@ -0,0 +1,5 @@
+var username = document.getElementById('username');
+var password = document.getElementById('password');
+function verifyAutentice() {
+}
+addEventListener('click', verifyAutentice);
